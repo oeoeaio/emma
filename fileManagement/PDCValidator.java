oeoeaio/emma

commit 5063b30e4224cac5c04aebe3d65b84dc6331db1d

Add concentrator number to management panes

fileManagement/PDCValidator.java
@@ -24,15 +24,10 @@ import java.util.TimeZone;
 
 import javax.swing.JOptionPane;
 
-import endUseWindow.Appliance;
-import endUseWindow.Circuit;
-import endUseWindow.Light;
 import endUseWindow.LogWindow;
 import endUseWindow.MySQLConnection;
-import endUseWindow.Phase;
 import endUseWindow.Site;
 import endUseWindow.Source;
-import endUseWindow.Temperature;
 
 public class PDCValidator implements Runnable{
 	
@@ -147,7 +142,7 @@ public class PDCValidator implements Runnable{
 							String fetchSiteIDSQL = "SELECT * FROM sites WHERE concentrator = '"+concSNList.get(i)+"' AND FROM_UNIXTIME("+(ALL_CONC_DATE_DATA.get(i).get(0)/1000)+") BETWEEN start_date AND end_date";
 							ResultSet siteRS = dbConn.createStatement().executeQuery(fetchSiteIDSQL);
 							if (siteRS.next()){
-								siteList[i] = new Site(siteRS.getString("site_id"),siteRS.getString("site_name"),siteRS.getString("given_name"),siteRS.getString("surname"),siteRS.getString("suburb"),siteRS.getString("state"));
+								siteList[i] = new Site(siteRS.getString("site_id"),siteRS.getString("site_name"),siteRS.getString("concentrator"),siteRS.getString("given_name"),siteRS.getString("surname"),siteRS.getString("suburb"),siteRS.getString("state"));
 							}
 							else{
 								siteList[i] = null;

endUseWindow/Site.java
@@ -5,14 +5,16 @@ import javax.swing.JOptionPane;
 public class Site {
 	String siteID;
 	String siteName;
+	String concentrator;
 	String givenName;
 	String surname;
 	String suburb;
 	String state;
 	
-	public Site(String siteID,String siteName,String givenName,String surname,String suburb,String state){
+	public Site(String siteID,String siteName,String concentrator,String givenName,String surname,String suburb,String state){
 		this.siteID = (siteID==null?"":siteID);
 		this.siteName = (siteName==null?"":siteName);
+		this.concentrator = (concentrator==null?"":concentrator);
 		this.givenName = (givenName==null?"":givenName);
 		this.surname = (surname==null?"":surname);
 		this.suburb = (suburb==null?"":suburb);
@@ -22,6 +24,7 @@ public class Site {
 	public boolean equalTo(Site otherSite){
 		//NOTE: IGNORES siteIDs. Only tests whether user added information is equal.
 		if (this.siteName.equals(otherSite.siteName)
+				&& this.concentrator.equals(otherSite.concentrator)
 				&& this.givenName.equals(otherSite.givenName)
 				&& this.surname.equals(otherSite.surname)
 				&& this.suburb.equals(otherSite.suburb)
@@ -37,26 +40,31 @@ public class Site {
 		boolean isValid = false;
 		//NOTE: IGNORES siteIDs. Only tests whether user added information is valid.
 		if (siteName.matches("^[\\w\\s]{1,10}$")){
-			if (givenName.matches("^[\\w\\s]{0,30}$")){
-				if (surname.matches("^[\\w\\s]{0,30}$")){
-					if (suburb.matches("^[\\w\\s]{0,30}$")){
-						if (state.matches("^VIC|TAS|ACT|NSW|QLD|NT|SA|WA$") || state.equals("")){
-							isValid = true;
-						}
+			if (concentrator.matches("^[\\d]{8}$")){
+				if (givenName.matches("^[\\w\\s]{0,30}$")){
+					if (surname.matches("^[\\w\\s]{0,30}$")){
+						if (suburb.matches("^[\\w\\s]{0,30}$")){
+							if (state.matches("^VIC|TAS|ACT|NSW|QLD|NT|SA|WA$") || state.equals("")){
+								isValid = true;
+							}
+							else{
+								JOptionPane.showMessageDialog(null,"The State provided is invalid.","Site Information Invalid",JOptionPane.WARNING_MESSAGE);
+							}
+						}	
 						else{
-							JOptionPane.showMessageDialog(null,"The State provided is invalid.","Site Information Invalid",JOptionPane.WARNING_MESSAGE);
+							JOptionPane.showMessageDialog(null,"The Suburb provided is invalid.","Site Information Invalid",JOptionPane.WARNING_MESSAGE);
 						}
-					}	
+					}
 					else{
-						JOptionPane.showMessageDialog(null,"The Suburb provided is invalid.","Site Information Invalid",JOptionPane.WARNING_MESSAGE);
+						JOptionPane.showMessageDialog(null,"The Surname provided is invalid.","Site Information Invalid",JOptionPane.WARNING_MESSAGE);
 					}
 				}
 				else{
-					JOptionPane.showMessageDialog(null,"The Surname provided is invalid.","Site Information Invalid",JOptionPane.WARNING_MESSAGE);
+					JOptionPane.showMessageDialog(null,"The Given Name provided is invalid.","Site Information Invalid",JOptionPane.WARNING_MESSAGE);
 				}
 			}
 			else{
-				JOptionPane.showMessageDialog(null,"The Given Name provided is invalid.","Site Information Invalid",JOptionPane.WARNING_MESSAGE);
+				JOptionPane.showMessageDialog(null,"The Concentrator No. provided is invalid.","Site Information Invalid",JOptionPane.WARNING_MESSAGE);
 			}
 		}
 		else{
@@ -76,6 +84,10 @@ public class Site {
 	
 	public String getSiteName(){
 		return siteName;
+	}
+	
+	public String getConcentrator(){
+		return concentrator;
 	}
 	
 	public String getGivenName(){

endUseWindow/SiteTable.java
@@ -45,7 +45,7 @@ public class SiteTable extends JTable{
 				if (siteRS.next()){
 					siteRS.beforeFirst(); //reset cursor position
 					while (siteRS.next()){
-						siteList.add(new Site(siteRS.getString("site_id"),siteRS.getString("site_name"),siteRS.getString("given_name"),siteRS.getString("surname"),siteRS.getString("suburb"),siteRS.getString("state")));
+						siteList.add(new Site(siteRS.getString("site_id"),siteRS.getString("site_name"),siteRS.getString("concentrator"),siteRS.getString("given_name"),siteRS.getString("surname"),siteRS.getString("suburb"),siteRS.getString("state")));
 					}
 					SwingUtilities.invokeLater(new PopulateTable());
 				}

endUseWindow/SourceTable.java
@@ -106,7 +106,7 @@ public class SourceTable extends JTable{
 				if (sourceRS.next()){
 					sourceRS.beforeFirst(); //reset cursor position
 					while (sourceRS.next()){
-						sourceList.add(new Source((site==null?new Site(sourceRS.getString("site_id"),sourceRS.getString("site_name"),sourceRS.getString("given_name"),sourceRS.getString("surname"),sourceRS.getString("suburb"),sourceRS.getString("state")):site),sourceRS.getString("source_id"),sourceRS.getString("source_name"),sourceRS.getString("source_type"),sourceRS.getString("measurement_type")));
+						sourceList.add(new Source((site==null?new Site(sourceRS.getString("site_id"),sourceRS.getString("site_name"),sourceRS.getString("concentrator"),sourceRS.getString("given_name"),sourceRS.getString("surname"),sourceRS.getString("suburb"),sourceRS.getString("state")):site),sourceRS.getString("source_id"),sourceRS.getString("source_name"),sourceRS.getString("source_type"),sourceRS.getString("measurement_type")));
 					}
 					SwingUtilities.invokeLater(new PopulateTable());
 				}

fileManagement/CTValidator.java
@@ -78,7 +78,7 @@ public class CTValidator implements Runnable{
 							ResultSet checkSiteNameRS = dbConn.createStatement().executeQuery(checkSiteNameSQL);
 							
 							if (checkSiteNameRS.next()){
-								Site testSite = new Site(checkSiteNameRS.getString("site_id"),checkSiteNameRS.getString("site_name"),checkSiteNameRS.getString("given_name"),checkSiteNameRS.getString("surname"),checkSiteNameRS.getString("suburb"),checkSiteNameRS.getString("state"));
+								Site testSite = new Site(checkSiteNameRS.getString("site_id"),checkSiteNameRS.getString("site_name"),checkSiteNameRS.getString("concentrator"),checkSiteNameRS.getString("given_name"),checkSiteNameRS.getString("surname"),checkSiteNameRS.getString("suburb"),checkSiteNameRS.getString("state"));
 								if (testSite.isValid()){
 									site = testSite;
 								}

fileManagement/SiteNotFoundWindow.java
@@ -44,11 +44,13 @@ public class SiteNotFoundWindow extends JFrame implements ActionListener {
 	JPanel o1InfoPanel = new JPanel(new BorderLayout());
 	JPanel o1InputPanel = new JPanel(new GridLayout(0,2));
 	JLabel o1SiteNameL = new JLabel("Site");
+	JLabel o1ConcentratorL = new JLabel("Concentrator:");
 	JLabel o1GivenNameL = new JLabel("Given Name");
 	JLabel o1SurnameL = new JLabel("Surname:");
 	JLabel o1SuburbL = new JLabel("Town/Suburb");
 	JLabel o1StateL = new JLabel("State");
 	JComboBox<String> o1SiteName = new JComboBox<String>();
+	JTextField o1Concentrator = new JTextField(10);
 	JTextField o1GivenName = new JTextField(10);
 	JTextField o1Surname = new JTextField(10);
 	JTextField o1Suburb = new JTextField(10);
@@ -59,11 +61,13 @@ public class SiteNotFoundWindow extends JFrame implements ActionListener {
 	JPanel o2InfoPanel = new JPanel(new BorderLayout());
 	JPanel o2InputPanel = new JPanel(new GridLayout(0,2));
 	JLabel o2SiteNameL = new JLabel("Site Name");
+	JLabel o2ConcentratorL = new JLabel("Concentrator:");
 	JLabel o2GivenNameL = new JLabel("Given Name");
 	JLabel o2SurnameL = new JLabel("Surname:");
 	JLabel o2SuburbL = new JLabel("Town/Suburb");
 	JLabel o2StateL = new JLabel("State");
 	JTextField o2SiteName = new JTextField(10);
+	JTextField o2Concentrator = new JTextField(10);
 	JTextField o2GivenName = new JTextField(10);
 	JTextField o2Surname = new JTextField(10);
 	JTextField o2Suburb = new JTextField(10);
@@ -126,6 +130,8 @@ public class SiteNotFoundWindow extends JFrame implements ActionListener {
 		
 		o1InputPanel.add(o1SiteNameL);
 		o1InputPanel.add(o1SiteName);
+		o1InputPanel.add(o1ConcentratorL);
+		o1InputPanel.add(o1Concentrator);
 		o1InputPanel.add(o1GivenNameL);
 		o1InputPanel.add(o1GivenName);
 		o1InputPanel.add(o1SurnameL);
@@ -140,7 +146,7 @@ public class SiteNotFoundWindow extends JFrame implements ActionListener {
 			if (allSites.next()){
 				allSites.beforeFirst();
 				while(allSites.next()){
-					siteList.add(new Site(allSites.getString("site_id"),allSites.getString("site_name"),allSites.getString("given_name"),allSites.getString("surname"),allSites.getString("suburb"),allSites.getString("state")));
+					siteList.add(new Site(allSites.getString("site_id"),allSites.getString("site_name"),allSites.getString("concentrator"),allSites.getString("given_name"),allSites.getString("surname"),allSites.getString("suburb"),allSites.getString("state")));
 					o1SiteName.addItem(allSites.getString("site_id")+": "+allSites.getString("site_name"));
 				}
 			}
@@ -155,6 +161,7 @@ public class SiteNotFoundWindow extends JFrame implements ActionListener {
 			o1SiteName.setEnabled(false);
 			o1SiteName.addItem("No Sites Found");
 		}
+		o1Concentrator.setEnabled(false);
 		o1GivenName.setEnabled(false);
 		o1Surname.setEnabled(false);
 		o1Suburb.setEnabled(false);
@@ -171,6 +178,8 @@ public class SiteNotFoundWindow extends JFrame implements ActionListener {
 		
 		o2InputPanel.add(o2SiteNameL);
 		o2InputPanel.add(o2SiteName);
+		o2InputPanel.add(o2ConcentratorL);
+		o2InputPanel.add(o2Concentrator);
 		o2InputPanel.add(o2GivenNameL);
 		o2InputPanel.add(o2GivenName);
 		o2InputPanel.add(o2SurnameL);
@@ -217,6 +226,7 @@ public class SiteNotFoundWindow extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent aE) {
 		if(aE.getSource().equals(o1SiteName) && siteList.size()>0){
 			Site selectedSite = siteList.get(o1SiteName.getSelectedIndex());
+			o1Concentrator.setText(selectedSite.getConcentrator());
 			o1GivenName.setText(selectedSite.getGivenName());
 			o1Surname.setText(selectedSite.getSurname());
 			o1Suburb.setText(selectedSite.getSuburb());
@@ -236,10 +246,10 @@ public class SiteNotFoundWindow extends JFrame implements ActionListener {
 					newSite = null;
 				}
 			} else if(o2Button.isSelected()){
-				Site testSite = new Site(null,o2SiteName.getText(),o2GivenName.getText(),o2Surname.getText(),o2Suburb.getText(),o2State.getText());
+				Site testSite = new Site(null,o2SiteName.getText(),o2Concentrator.getText(),o2GivenName.getText(),o2Surname.getText(),o2Suburb.getText(),o2State.getText());
 				if (testSite.isValid()){
 					try{
-						dbConn.createStatement().executeUpdate("INSERT INTO sites (site_name,given_name,surname,suburb,state) VALUES("+(testSite.getSiteName().equals("")?"NULL":"'"+testSite.getSiteName()+"'")+","+(testSite.getGivenName().equals("")?"NULL":"'"+testSite.getGivenName()+"'")+","+(testSite.getSurname().equals("")?"NULL":"'"+testSite.getSurname()+"'")+","+(testSite.getSuburb().equals("")?"NULL":"'"+testSite.getSuburb()+"'")+","+(testSite.getState().equals("")?"NULL":"'"+testSite.getState()+"'"));
+						dbConn.createStatement().executeUpdate("INSERT INTO sites (site_name,concentrator,given_name,surname,suburb,state) VALUES("+(testSite.getSiteName().equals("")?"NULL":"'"+testSite.getSiteName()+"'")+","+(testSite.getConcentrator().equals("")?"NULL":"'"+testSite.getConcentrator()+"'")+","+(testSite.getGivenName().equals("")?"NULL":"'"+testSite.getGivenName()+"'")+","+(testSite.getSurname().equals("")?"NULL":"'"+testSite.getSurname()+"'")+","+(testSite.getSuburb().equals("")?"NULL":"'"+testSite.getSuburb()+"'")+","+(testSite.getState().equals("")?"NULL":"'"+testSite.getState()+"'"));
 						logWindow.println("Added new site with name '"+testSite.getSiteName()+"'.");
 						ResultSet new_source_id = dbConn.createStatement().executeQuery("SELECT LAST_INSERT_ID() AS current_id"); //returns new id
 						new_source_id.next();

fileManagement/StandAloneBatchImporter.java
@@ -69,7 +69,7 @@ public class StandAloneBatchImporter implements Runnable{
 				for (int i=0;i<batchInfo.size();i++){
 					try {
 						//create new test site and test source
-						Site testSite = new Site(null,batchInfo.get(i).data[0],batchInfo.get(i).data[1],batchInfo.get(i).data[2],batchInfo.get(i).data[3],batchInfo.get(i).data[4]);
+						Site testSite = new Site(null,batchInfo.get(i).data[0],null,batchInfo.get(i).data[1],batchInfo.get(i).data[2],batchInfo.get(i).data[3],batchInfo.get(i).data[4]);
 						Source testSource = new Source(testSite,null,batchInfo.get(i).data[5],batchInfo.get(i).data[6],batchInfo.get(i).data[7]);
 						DataFile dataFile = new DataFile();
 						//if test site and source do not match existing test site and source, write data for previous source
@@ -97,7 +97,7 @@ public class StandAloneBatchImporter implements Runnable{
 							if (existingSiteInfo.next()){
 								existingSiteID = existingSiteInfo.getString("site_id");
 								dataFile.siteID = existingSiteID;
-								existingSite = new Site(existingSiteID,existingSiteInfo.getString("site_name"),existingSiteInfo.getString("given_name"),existingSiteInfo.getString("surname"),existingSiteInfo.getString("suburb"),existingSiteInfo.getString("state"));
+								existingSite = new Site(existingSiteID,existingSiteInfo.getString("site_name"),null,existingSiteInfo.getString("given_name"),existingSiteInfo.getString("surname"),existingSiteInfo.getString("suburb"),existingSiteInfo.getString("state"));
 
 								if (testSite.equalTo(existingSite)==false){
 									logWindow.printString("WARNING: site "+testSite.getSiteName()+" mismatch. ");
@@ -127,7 +127,7 @@ public class StandAloneBatchImporter implements Runnable{
 								new_site_id.next();
 								existingSiteID = new_site_id.getString("current_id");
 								dataFile.siteID = existingSiteID;
-								existingSite = new Site(existingSiteID,testSite.getSiteName(),testSite.getGivenName(),testSite.getSurname(),testSite.getSuburb(),testSite.getState());
+								existingSite = new Site(existingSiteID,testSite.getSiteName(),null,testSite.getGivenName(),testSite.getSurname(),testSite.getSuburb(),testSite.getState());
 
 								logWindow.println("Added new site ID "+existingSiteID);
 							}

commit 64f4400bc9175dd25f69590d461b202789c20b87

Fixed bug which caused files after erroneous files to be ignored in PDCValidator. Caused in 5063b30e4224cac5c04aebe3d65b84dc6331db1d

fileManagement/PDCValidator.java
@@ -145,6 +145,9 @@ public class PDCValidator implements Runnable{
 						if (fileData.validDataExtracted){
 							validateForDatabase(fileFeeder,fileList.get(i),fileData);
 						}
+						else{
+							i--;
+						}
 					}
 				}
 				
@@ -817,7 +820,7 @@ public class PDCValidator implements Runnable{
 							return fileData;
 						}
 						else{
-							logWindow.println("Missing module data blocks or headers from file: "+fileList.get(filePointer).getName()+". This file will be ignored.");
+							logWindow.println("Missing module data blocks or headers from file: "+fileList.get(filePointer).getName()+". This file will be ignored.\r\n");
 							logWindow.println((Integer.parseInt(concHeaderValues[5])-1)+" "+ctModDataBlockHeaderValues.size());
 							fileIssueList.add(new FileIssue(fileList.get(filePointer),"ModData",""));
 							fileList.remove(filePointer);
@@ -825,14 +828,14 @@ public class PDCValidator implements Runnable{
 						}
 					}
 					else{
-						logWindow.println("Corrupted or missing concentrator data blocks in file: "+fileList.get(filePointer).getName()+". This file will be ignored.");
+						logWindow.println("Corrupted or missing concentrator data blocks in file: "+fileList.get(filePointer).getName()+". This file will be ignored.\r\n");
 						fileIssueList.add(new FileIssue(fileList.get(filePointer),"ConcData",""));
 						fileList.remove(filePointer);
 						return new FileData(null,null,null,null,null,null,null,null,false);
 					}
 				}
 				else {
-					logWindow.println("File: "+fileList.get(filePointer).getName()+" is missing Headers. This file will be ignored. ");
+					logWindow.println("File: "+fileList.get(filePointer).getName()+" is missing Headers. This file will be ignored.\r\n");
 					fileIssueList.add(new FileIssue(fileList.get(filePointer),"MissHead",""));
 					fileList.remove(filePointer);
 					return new FileData(null,null,null,null,null,null,null,null,false);
@@ -840,14 +843,14 @@ public class PDCValidator implements Runnable{
             }
 			else {
 				logWindow.println(blocksOK[1]);
-				logWindow.println("Ignored corrupted file: "+fileList.get(filePointer).getName());
+				logWindow.println("Ignored corrupted file: "+fileList.get(filePointer).getName()+"\r\n");
 				fileIssueList.add(new FileIssue(fileList.get(filePointer),"Corrupt",""));
 				fileList.remove(filePointer);
 				return new FileData(null,null,null,null,null,null,null,null,false);
 			}
 		}   
 		else{
-			logWindow.println("Ignored empty file "+fileList.get(filePointer).getName());
+			logWindow.println("Ignored empty file "+fileList.get(filePointer).getName()+"\r\n");
 			fileIssueList.add(new FileIssue(fileList.get(filePointer),"Empty",""));
 			fileList.remove(filePointer);
 			return new FileData(null,null,null,null,null,null,null,null,false);